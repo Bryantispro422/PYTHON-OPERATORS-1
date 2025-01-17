6bbb05ea08f006b6aad448fc35645fe362285d62
Bryantispro422 <cnbeetseh@gmail.com>
2025-01-17 20:29:57 +0100

    arith1stcommit

---
+++ ARIMthicoperator.py
@@ -0,0 +1,9 @@
+tree1=100
+tree2=100
+tree3=100
+tree4=100
+tree5=100
+sum=tree1+tree2+tree3+tree4+tree5
+print("the sum of all trees is ",sum)
+average=sum/5
+print("the average of all the trees is",average)
